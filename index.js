52b4bfce801fdcca680f66469499d42beabd030a
Gabriel Henrique <htm.gabriel@gmail.com>
2021-09-01 11:27:40 -0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Teste')
+const express       = require('express')
+const bodyParser    = require('body-parser')
+const cors          = require('cors')
+const moment        = require('moment')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use((req, res, next) => {
+	((req.headers['x-forwarded-proto'] || '').endsWith('http')) ? res.redirect(`https://${req.hostname}${req.url}`) : next()
+})
+
+app.get('/', (req, res) => {
+	res.status(200).json({message: 'OK'});
+})
+
+app.listen(3000, async () => {
+	console.log('Server running')
+})
