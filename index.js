5c9ff7bebbd87d9b39e577c37ff83319f5eff6ff
Gabriel Henrique <43080243+htgabriel@users.noreply.github.com>
2021-09-01 14:54:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-	res.status(200).json({message: 'TESTE'});
+	res.status(200).json({message: 'Teste'});
 })
 
 app.listen(3000, async () => {
